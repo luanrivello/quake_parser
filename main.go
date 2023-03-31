23069ae012d8a6dd7e85be436724a39c07518397
luanrivello <luanrivello@hotmail.com>
2023-03-31 09:09:56 -0300

    getPath function created

--- main.go
+++ main.go
@@ -8,13 +8,17 @@ import (
 func main() {
 
 	// Path to the log file
-	var path string
-	if len(os.Args) > 1 {
-		path = strings.Join(os.Args[1:], " ")
-	} else {
-		path = "./data/qgames.log"
-	}
-
+	var path string = getPath()
 	println(path)
+
+}
+
+func getPath() string {
+
+	if len(os.Args) > 1 {
+		return strings.Join(os.Args[1:], " ")
+	} else {
+		return "./data/qgames.log"
+	}
 
 }
