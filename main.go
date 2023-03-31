4775e7f6a1608da909e2c8ab2e6f09e3e8a7eaa4
luanrivello <luanrivello@hotmail.com>
2023-03-31 09:28:18 -0300

    get log contents

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -10,6 +13,11 @@ func main() {
 	// Path to the log file
 	var path string = getPath()
 	println(path)
+
+	var content string = getContent(path)
+
+	// Print the file contents
+	fmt.Println(string(content))
 
 }
 
@@ -20,5 +28,22 @@ func getPath() string {
 	} else {
 		return "./data/qgames.log"
 	}
+
+}
+
+func getContent(path string) string {
+
+	fileAbsPath, err := filepath.Abs(path)
+	if err != nil {
+		fmt.Println("Error getting file path:", err)
+	}
+
+	// Get file contents
+	content, err := ioutil.ReadFile(fileAbsPath)
+	if err != nil {
+		fmt.Println("Error reading file:", err)
+	}
+
+	return string(content)
 
 }
