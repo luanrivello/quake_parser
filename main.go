5f0bd8888713a95319b35d28b97996b0e1d19878
luanrivello <luanrivello@hotmail.com>
2023-03-31 08:57:58 -0300

    get path to the log file

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"os"
+	"strings"
+)
+
 func main() {
-	println("Hello world!")
+
+	// Path to the log file
+	var path string
+	if len(os.Args) > 1 {
+		path = strings.Join(os.Args[1:], " ")
+	} else {
+		path = "./data/qgames.log"
+	}
+
+	println(path)
+
 }
