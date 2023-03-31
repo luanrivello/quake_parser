14233d7c9a3716027eec17d7ce0e522f6a8068be
luanrivello <luanrivello@hotmail.com>
2023-03-31 18:42:23 -0300

    Extract player name

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 	"sync"
 )
@@ -18,26 +20,32 @@ func Parse(log string) {
 
 	lines := strings.Split(log, "\n")
 
+	//* Iterate log lines
 	for lineNumber, line := range lines {
 		line := strings.TrimSpace(line)
 		tokens := strings.Split(line, " ")
 
+		//* Find next match
 		if len(tokens) > 2 {
 			if tokens[1] == "InitGame:" {
 				waitgroup.Add(1)
 				matchNumber++
 				go extractMatch(lines, lineNumber+1, matchNumber, &waitgroup)
+				if matchNumber == 2 {
+					break
+				}
 			}
 		}
 	}
 
+	//* Wait processes
 	waitgroup.Wait()
 }
 
 func extractMatch(lines []string, lineNumber int, matchNumber int, waitgroup *sync.WaitGroup) Match {
 	defer waitgroup.Done()
 
-	// Match data
+	//* Match data
 	var match Match = Match{
 		id:         matchNumber,
 		totalKills: 0,
@@ -45,15 +53,31 @@ func extractMatch(lines []string, lineNumber int, matchNumber int, waitgroup *sy
 		killCount:  make(map[string]int),
 	}
 
+	//* Log lines form specific match
 	for lineNumber < len(lines) {
 		line := strings.TrimSpace(lines[lineNumber])
 		tokens := strings.Split(line, " ")
 		if len(tokens) > 1 {
+			//* Kill data
 			if tokens[1] == "Kill:" {
-				//println(line)
 				match.totalKills++
+
+				//* Player name
+			} else if tokens[1] == "ClientUserinfoChanged:" {
+				//fmt.Println(tokens[3])
+				regex := regexp.MustCompile(`\\(\w+|\w+ )+\\`) 
+				player := regex.FindString(strings.Join(tokens[3:], " "))
+				if len(player) > 1 {
+					player := strings.ReplaceAll(player, "\\", "")
+					println(player)
+				} else {
+					fmt.Println("No match found")
+				}
+				// extract name
+				// check if exists
+
+				//* End of match
 			} else if tokens[1] == "InitGame:" {
-				//println(line)
 				println("Match", match.id, "TotalKills", match.totalKills)
 				return match
 			}
@@ -61,5 +85,6 @@ func extractMatch(lines []string, lineNumber int, matchNumber int, waitgroup *sy
 		lineNumber++
 	}
 
+	println("Match", match.id, "TotalKills", match.totalKills)
 	return match
 }
