32d5f8786a0d87cb0c93c83d7c767453d19f020f
luanrivello <luanrivello@hotmail.com>
2023-04-01 20:42:15 -0300

    contemplate player ranking

--- parser/reporter.go
+++ parser/reporter.go
@@ -7,6 +7,7 @@ import (
 
 func Write(matchs map[string]*Match) {
 	createJsonReport(matchs)
+	createJsonRank(matchs)
 }
 
 func createJsonReport(matchs map[string]*Match) {
@@ -16,7 +17,51 @@ func createJsonReport(matchs map[string]*Match) {
 		return
 	}
 
-	file, err := os.Create("report/report.json")
+	file, err := os.Create("report/grouped_information.json")
+	if err != nil {
+		println("Error creating JSON file:", err)
+		return
+	}
+	defer file.Close()
+
+	_, err = file.Write(jsonData)
+	if err != nil {
+		println("Error writing JSON to file:", err)
+		return
+	}
+}
+
+type rank struct {
+	leaderboard map[int]string
+}
+
+func newRank(match Match) rank {
+	var aux int = 1
+	var result rank = rank{
+		leaderboard: make(map[int]string),
+	}
+
+	for player, _ := range match.KillCount {
+		result.leaderboard[aux] = player
+	}
+
+	return result
+}
+
+func createJsonRank(matchs map[string]*Match) {
+	var ranks map[string]rank = make(map[string]rank)
+
+	for matchName, match := range matchs {
+		ranks[matchName] = newRank(*match)
+	}
+
+	jsonData, err := json.MarshalIndent(ranks, "", "  ")
+	if err != nil {
+		println("Error marshalling to JSON:", err)
+		return
+	}
+
+	file, err := os.Create("report/player_ranking.json")
 	if err != nil {
 		println("Error creating JSON file:", err)
 		return
