35386213b0470eca824172d3ff909f4e762b2d7a
luanrivello <luanrivello@hotmail.com>
2023-04-02 20:32:47 -0300

    reporter unit test

---
+++ parser_test/reporter_test.go
@@ -0,0 +1,60 @@
+package parser_test
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"testing"
+
+	"quake_parser/parser"
+)
+
+func TestWrite(t *testing.T) {
+	//* Create test data
+	match := &parser.Match{
+		TotalKills: 10,
+		Players:    []string{"Player1", "Player2"},
+		KillCount: map[string]int{
+			"Player1": 5,
+			"Player2": 3,
+		},
+		Leaderboard: map[int]string{
+			1: "Player1",
+			2: "Player2",
+		},
+		KillMeans: map[string]int{
+			"MOD_SHOTGUN": 5,
+			"MOD_ROCKET":  3,
+		},
+	}
+
+	//* Create a test matches map
+	matches := make(map[string]*parser.Match)
+	matches["test_match"] = match
+
+	//* Write the test matches to the temporary file
+	tempPath := "./report/report.json"
+	parser.Write(matches)
+
+	//* Read the contents of the temporary file
+	fileData, err := ioutil.ReadFile(tempPath)
+	if err != nil {
+		t.Fatalf("Error reading temp file: %v", err)
+	}
+
+	//* Unmarshal the JSON data
+	var parsedData map[string]*parser.Match
+	err = json.Unmarshal(fileData, &parsedData)
+	if err != nil {
+		t.Fatalf("Error unmarshalling JSON data: %v", err)
+	}
+
+	//* Compare the parsed data to the original matches
+	if !reflect.DeepEqual(parsedData, matches) {
+		t.Fatalf("Parsed data does not match original matches:\n%v\n%v", parsedData, matches)
+	}
+
+	//* Remove test report
+	os.Remove(tempPath)
+}
