35c1a1c00186469e9210c6488ee3ab8fa71fcdc2
luanrivello <luanrivello@hotmail.com>
2023-04-03 08:15:44 -0300

    added more test cases to RegisterKill

--- parser_test/parser_test.go
+++ parser_test/parser_test.go
@@ -37,38 +37,104 @@ func TestNewLeaderboard(t *testing.T) {
 }
 
 func TestRegisterKill(t *testing.T) {
-	//* Create test data
-	match := &parser.Match{
-		TotalKills:  0,
-		Players:     []string{"Player1", "Player2", "Player3"},
-		KillCount:   map[string]int{},
-		Leaderboard: map[int]string{},
-		KillMeans:   map[string]int{},
-	}
+	t.Run("Test player kill", func(t *testing.T) {
+		//* Create test data
+		match := &parser.Match{
+			TotalKills:  0,
+			Players:     []string{"Player1", "Player2", "Player3"},
+			KillCount:   map[string]int{},
+			Leaderboard: map[int]string{},
+			KillMeans:   map[string]int{},
+		}
 
-	line := "22:18 Kill: 2 2 7: Player1 killed Player2 by MOD_RAILGUN"
-	tokens := strings.Split(line, " ")
+		line := "22:18 Kill: 2 2 7: Player1 killed Player2 by MOD_RAILGUN"
+		tokens := strings.Split(line, " ")
 
-	//* FUNCTION CALL
-	parser.RegisterKill(match, tokens)
+		//* FUNCTION CALL
+		parser.RegisterKill(match, tokens)
 
-	//* Check if total kills increased
-	if match.TotalKills != 1 {
-		t.Errorf("registerKill did not increment TotalKills. Got %v, expected %v", match.TotalKills, 1)
-	}
+		//* Check if total kills increased
+		if match.TotalKills != 1 {
+			t.Errorf("registerKill did not increment TotalKills. Got %v, expected %v", match.TotalKills, 1)
+		}
 
-	//* Check if killer's kill count increased
-	if match.KillCount["Player1"] != 1 {
-		t.Errorf("registerKill did not register a kill for the killer. Got %v, expected %v", match.KillCount["Player1"], 1)
-	}
+		//* Check if killer's kill count increased
+		if match.KillCount["Player1"] != 1 {
+			t.Errorf("registerKill did not register a kill for the killer. Got %v, expected %v", match.KillCount["Player1"], 1)
+		}
 
-	//* Check if victim's kill count remained the same
-	if match.KillCount["Player2"] != 0 {
-		t.Errorf("registerKill subtracted a kill from <world> instead of adding it to the victim. Got %v, expected %v", match.KillCount["Player2"], 0)
-	}
+		//* Check if victim's kill count remained the same
+		if match.KillCount["Player2"] != 0 {
+			t.Errorf("registerKill subtracted a kill from <world> instead of adding it to the victim. Got %v, expected %v", match.KillCount["Player2"], 0)
+		}
 
-	//* Check if kill means were updated
-	if match.KillMeans["MOD_RAILGUN"] != 1 {
-		t.Errorf("registerKill did not register the correct kill mean. Got %v, expected %v", match.KillMeans["MOD_RIFLE"], 1)
-	}
+		//* Check if kill means were updated
+		if match.KillMeans["MOD_RAILGUN"] != 1 {
+			t.Errorf("registerKill did not register the correct kill mean. Got %v, expected %v", match.KillMeans["MOD_RIFLE"], 1)
+		}
+	})
+
+	t.Run("Test world kill", func(t *testing.T) {
+		//* Create test data
+		match := &parser.Match{
+			TotalKills:  0,
+			Players:     []string{"Player1", "Player2", "Player3"},
+			KillCount:   map[string]int{},
+			Leaderboard: map[int]string{},
+			KillMeans:   map[string]int{},
+		}
+
+		line := "22:18 Kill: 2 2 7: <world> killed Player2 by MOD_FALLING"
+		tokens := strings.Split(line, " ")
+
+		//* FUNCTION CALL
+		parser.RegisterKill(match, tokens)
+
+		//* Check if total kills increased
+		if match.TotalKills != 1 {
+			t.Errorf("registerKill did not increment TotalKills. Got %v, expected %v", match.TotalKills, 1)
+		}
+
+		//* Check if victim's kill count decreased
+		if match.KillCount["Player2"] != -1 {
+			t.Errorf("registerKill didnt subtracted a kill from <world>. Got %v, expected %v", match.KillCount["Player2"], 0)
+		}
+
+		//* Check if kill means were updated
+		if match.KillMeans["MOD_FALLING"] != 1 {
+			t.Errorf("registerKill did not register the correct kill mean. Got %v, expected %v", match.KillMeans["MOD_RIFLE"], 1)
+		}
+	})
+
+	t.Run("Test suicide kill", func(t *testing.T) {
+		//* Create test data
+		match := &parser.Match{
+			TotalKills:  0,
+			Players:     []string{"Player1", "Player2", "Player3"},
+			KillCount:   map[string]int{},
+			Leaderboard: map[int]string{},
+			KillMeans:   map[string]int{},
+		}
+
+		line := "22:18 Kill: 2 2 7: Player2 killed Player2 by MOD_ROCKET_SPLASH"
+		tokens := strings.Split(line, " ")
+
+		//* FUNCTION CALL
+		parser.RegisterKill(match, tokens)
+
+		//* Check if total kills increased
+		if match.TotalKills != 1 {
+			t.Errorf("registerKill did not increment TotalKills. Got %v, expected %v", match.TotalKills, 1)
+		}
+
+		//* Check if killer's kill count increased
+		if match.KillCount["Player2"] != 1 {
+			t.Errorf("registerKill did not register a kill for the killer. Got %v, expected %v", match.KillCount["Player1"], 1)
+		}
+
+		//* Check if kill means were updated
+		if match.KillMeans["MOD_SUICIDE"] != 1 {
+			t.Errorf("registerKill did not register the correct kill mean. Got %v, expected %v", match.KillMeans["MOD_RIFLE"], 1)
+		}
+	})
 }
